45daa2c9a6110273c1742eb7727b38797640b234
Camilo Andrés Macias Vargas <Camilo Andrs Macias Vargas>
2016-10-22 20:52:42 +0300

    Clean Repository to upload new project

--- VaadinShoppingCart/src/main/java/VaadinProject/com/vaadin/shoppingcart/dto/Clothings.java
+++
@@ -1,9 +0,0 @@
-package VaadinProject.com.vaadin.shoppingcart.dto;
-
-import VaadinProject.com.vaadin.shoppingcart.component.ShoppingCartItem;
-
-/**
- * Created by keihell on 14/10/2016.
- */
-public class Clothings extends ShoppingCartItem {
-}
